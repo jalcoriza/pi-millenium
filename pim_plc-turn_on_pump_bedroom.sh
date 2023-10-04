0d0e9e82b325575b4c3de460d9d13cdf93bbd249
Javier Alcoriza <jalcoriza@gmail.com>
2023-10-04 12:27:52 +0200

    added pim_plc*.sh scripts

---
+++ pim_plc-turn_on_pump_bedroom.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# working variables
+#DATEVAR=date +%Y%m%d_%H%M
+CURRENT_DATE=$(date +%Y%m%d_%H%M%S)
+VERSION_VAR='(v1.0)'
+LOG_FILE='/home/pi/Projects/pi-millenium/pi-millenium.log'
+CMD_FILE='/home/pi/Projects/pi-millenium/command2.csv'
+
+echo $CURRENT_DATE $VERSION_VAR $0 >> $LOG_FILE
+echo 'r,nod,11,1' >> $CMD_FILE
